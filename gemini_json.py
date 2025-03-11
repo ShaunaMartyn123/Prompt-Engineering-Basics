bceeb53513af65c6e25087e5509ffe9975aaad42
ShaunaMartyn123 <shauna.martyn@zinkworks.com>
2025-03-11 16:43:27 +0000

    Update gemini_json.py

--- gemini_json.py
+++ gemini_json.py
@@ -2,7 +2,7 @@ from google import genai
 import json  # Import JSON module
 
 # Initialize Gemini client
-client = genai.Client(api_key="AIzaSyBh5dES-rcu14U8DWCJIxKbx0mKhjrA1l0")  
+client = genai.Client(api_key="ADD GOOGLE AI STUDIO API KEY HERE ")  
 
 # Define a structured prompt
 prompts = {
